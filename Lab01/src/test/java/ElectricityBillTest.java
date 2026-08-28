74e53c56c7a29c725d30e33130e03c6a99b31f23
hoangnguyen3101 <nguyenvanhoang135790@gmail.com>
2026-08-28 08:29:01 +0700

    test-add JUnit tests for statement coverage

---
+++ Lab01/src/test/java/ElectricityBillTest.java
@@ -0,0 +1,32 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ElectricityBillTest {
+
+    @Test
+    void testUsageBelowOrEqual50() {
+        double[] usages = {30};
+
+        double result = ElectricityBill.calculateBill(usages);
+
+        assertEquals(54.0, result, 0.001);
+    }
+
+    @Test
+    void testUsageAbove50() {
+        double[] usages = {60};
+
+        double result = ElectricityBill.calculateBill(usages);
+
+        assertEquals(115.0, result, 0.001);
+    }
+
+    @Test
+    void testMultipleUsages() {
+        double[] usages = {30, 60, 40};
+
+        double result = ElectricityBill.calculateBill(usages);
+
+        assertEquals(241.0, result, 0.001);
+    }
+}
